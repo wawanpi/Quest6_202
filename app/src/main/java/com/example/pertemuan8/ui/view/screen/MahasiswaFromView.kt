e6b4105bb2067d0c6ac800699a6e88ce847888f7
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 01:11:35 +0700

    Menambahkan dipendensis implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6") implementation("androidx.navigation:navigation-compose:2.8.3")

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFromView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFromView.kt
@@ -1,10 +1,15 @@
 package com.example.pertemuan8.ui.view.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.colorResource
+import com.example.pertemuan8.R
+import java.lang.reflect.Modifier
 
 @Composable
 fun MahasiswaFormView(
@@ -15,4 +20,12 @@ fun MahasiswaFormView(
     var nim by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var listData: MutableList<String> = mutableListOf(nama, nim, email)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        
+    }
 }
