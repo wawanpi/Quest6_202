db866fd051ac2f4e895a24190fa09991dff5707a
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 02:09:13 +0700

    Menambahakan gambar dan membuat container column

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFromView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFromView.kt
@@ -1,6 +1,12 @@
 package com.example.pertemuan8.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,7 +15,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.colorResource
 import com.example.pertemuan8.R
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun MahasiswaFormView(
@@ -23,9 +32,15 @@ fun MahasiswaFormView(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = colorResource(id = R.color.primary)),
+            .background(color = colorResource(id = R.color.teal_700)),
         horizontalAlignment = Alignment.CenterHorizontally,
     ){
-        
+        Spacer(modifier = Modifier.padding(16.dp))
+        Image(
+            painter = painterResource(id = R.drawable.umy),
+            contentDescription = "",
+            modifier = Modifier
+                .size(45.dp)
+        )
     }
 }
