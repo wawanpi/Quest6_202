a318315bd2f0d09ecbb8a3b81de2ef0c8bf97c11
wawanpi <muhaguskurniawan@gmail.com>
2024-11-21 23:11:17 +0700

    Membuat object MataKuliah.kt

---
+++ app/src/main/java/com/example/pertemuan8/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.pertemuan8.data
+
+object MataKuliah {
+    val option = listOf(
+    "Pemrogman Aplikasi Mobile Lanjut",
+    "Pengembangan Aplikasi Web Framework",
+    "Prototyping",
+    "Aplikasi Multimedia",
+    "Kecerdasan Buatan",
+    "Computer Vission"
+    )
+}
