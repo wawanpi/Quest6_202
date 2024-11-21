03f190e45615eefa01131af308be0d35e8f592a8
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 04:08:51 +0700

    membuat ontainer column yang terdapat dua text dan di dalam colum terdapat box

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFromView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFromView.kt
@@ -2,11 +2,15 @@ package com.example.pertemuan8.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,8 +20,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.colorResource
 import com.example.pertemuan8.R
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -34,13 +41,44 @@ fun MahasiswaFormView(
             .fillMaxSize()
             .background(color = colorResource(id = R.color.teal_700)),
         horizontalAlignment = Alignment.CenterHorizontally,
-    ){
+    ) {
         Spacer(modifier = Modifier.padding(16.dp))
-        Image(
-            painter = painterResource(id = R.drawable.umy),
-            contentDescription = "",
+        Row(verticalAlignment = Alignment.CenterVertically,) {
+
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(45.dp)
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    "Univeersitas Muhammadiyah Yogyakarta",
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "Unggul dan Islami",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
+        Spacer(modifier = Modifier.padding(16.dp))
+        Box(
             modifier = Modifier
-                .size(45.dp)
-        )
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topStart = 15.dp,
+                        topEnd = 15.dp
+                    )
+                )
+                .fillMaxSize(),
+            ){
+
+        }
     }
 }
