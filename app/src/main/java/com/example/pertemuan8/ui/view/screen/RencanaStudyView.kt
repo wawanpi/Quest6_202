8538a49ff8f839df3a05a4fe691bcb1197d7239a
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 05:02:56 +0700

    Membuat container column yang mana di dalam column memiliki row dan di dalam rao ada gamabar

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
@@ -1,9 +1,25 @@
 package com.example.pertemuan8.ui.view.screen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan8.R
 
 @Composable
 fun RencanaStudyView(
@@ -14,4 +30,25 @@ fun RencanaStudyView(
 var chosenDropdown by remember { mutableStateOf("") }
     var cheked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.teal_700))
+    ){
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+        }
+    }
 }
