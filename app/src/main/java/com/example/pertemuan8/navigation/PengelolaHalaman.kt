78d56395eeaa29f0297944e45b9daf9dd371a133
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 13:50:27 +0700

    Membuat fungsi MahasiswaApp

--- app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
@@ -1,8 +1,24 @@
 package com.example.pertemuan8.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan8.ui.view.viewmodel.MahasiswaViewModel
+import com.example.pertemuan8.ui.view.viewmodel.RencanaStudyViewModel
+
 enum class Halaman{
     Splash,
     Mahasiswa,
     MataKuliah,
     Tampilan
 }
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    KrsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
