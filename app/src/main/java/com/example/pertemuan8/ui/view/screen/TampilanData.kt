11ce9d70c55e09ffc2fb2b0ad21904c643d878fa
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 14:41:50 +0700

    membuat container box dan  yang mana di dalam box ada box

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/TampilanData.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/TampilanData.kt
@@ -1,13 +1,20 @@
 package com.example.pertemuan8.ui.view.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -56,7 +63,26 @@ fun TampilData(
                 fontSize = 12.sp,
                 color = Color.White
             )
-
         }
+        Box{
+            Icon(
+                imageVector = Icons.Filled.Notifications,
+                contentDescription = "",
+                tint = Color.White
+            )
+        }
+    }
+    Box(
+        modifier = Modifier
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(
+                    topStart = 20.dp,
+                    topEnd = 20.dp
+                )
+            )
+            .fillMaxSize()
+    ){
+
     }
 }
