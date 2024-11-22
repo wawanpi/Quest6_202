90affde3ff58ca4118607cdc193d83755afa0971
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 14:01:07 +0700

    Membuat NavHost

--- app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan8/navigation/PengelolaHalaman.kt
@@ -1,9 +1,14 @@
 package com.example.pertemuan8.navigation
 
+import android.window.SplashScreenView
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.pertemuan8.ui.view.viewmodel.MahasiswaViewModel
 import com.example.pertemuan8.ui.view.viewmodel.RencanaStudyViewModel
@@ -22,3 +27,17 @@ fun MahasiswaApp(
     KrsViewModel: RencanaStudyViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ){
+    val mahasiwaUiState =mahasiswaViewModel.MahasiswaStateUI.collectAsState().value
+    val KrsUiState = KrsViewModel.krsState.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = modifier.padding()
+    ){
+        composable(route = Halaman.Splash.name){
+            SplashView(onMulaiButton = {
+                navController.navigate(Halaman.Mahasiswa.name)
+            })
+        }
+    }
+}
