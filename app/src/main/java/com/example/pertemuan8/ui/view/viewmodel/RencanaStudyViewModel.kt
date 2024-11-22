7e7020e19b0d80e79f52ce6487339db6a12e07e7
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 15:35:20 +0700

    Membuat class RencanaStudyViewModel dan membuat fungsi savedataKRS

--- app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.pertemuan8.ui.view.viewmodel
 
-class RencanaStudyViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel: ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update {stateMK -> stateMK.copy(namaMk = mkPilihan)}
+    }
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+    fun saveDataKRS(ls:MutableList<String>){
+        _krsState.update { status -> status.copy(
+            namaMk = ls[0],
+            kelas = ls[1]
+        )
+        }
+    }
 }
