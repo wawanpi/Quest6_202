22a90218a01bc3d0b845fa2d53d0ce23452ac8a2
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 12:59:03 +0700

    Membuat class MahasiswaViewModel.kt

--- app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,24 @@
-package com.example.pertemuan8.ui.view.viewmodel;
+package com.example.pertemuan8.ui.view.viewmodel
 
-public class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+    private val _mahasiwaStateUI = MutableStateFlow(Mahasiswa())
+
+    val MahasiswaStateUI: StateFlow<Mahasiswa> = _mahasiwaStateUI.asStateFlow()
+
+    fun saveDataMahasiswa(ls: MutableList<String>) {
+        _mahasiwaStateUI.update { stateSaatIni ->
+            stateSaatIni.copy(
+                nama = ls[0],
+                nim = ls[1],
+                email = ls[2]
+            )
+        }
+    }
 }
