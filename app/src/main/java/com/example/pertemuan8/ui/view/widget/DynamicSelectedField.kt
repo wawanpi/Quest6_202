55b845767613eb1537fea242abc919c5c362c454
wawanpi <muhaguskurniawan@gmail.com>
2024-11-22 14:18:41 +0700

    Membuat fun DynamicSelectedTextField dan menambahkan modifier

--- app/src/main/java/com/example/pertemuan8/ui/view/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/widget/DynamicSelectedField.kt
@@ -1,2 +1,17 @@
 package com.example.pertemuan8.ui.view.widget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DynamicSelectedTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier =Modifier
+){
+
+}
